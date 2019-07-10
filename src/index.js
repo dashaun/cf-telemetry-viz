0ad7545e31b911dce2bd3ad81db9d8ea190189eb
Mike McDearmon <pair+mmcdearmon@pivotallabs.com>
2019-07-10 16:40:37 -0400

    adjust davi dark theme hover state color and remove unused PUI components

--- src/index.js
+++ src/index.js
@@ -3,22 +3,13 @@ import ReactDOM from 'react-dom';
 import AppUsage from './app_usage.js';
 import ServiceUsage from './service_usage.js';
 import {Siteframe} from 'pivotal-ui/react/siteframe';
-import {Grid, FlexCol} from 'pivotal-ui/react/flex-grids';
-import {Icon} from 'pivotal-ui/react/iconography';
 import 'pivotal-ui/css/typography';
 import 'pivotal-ui/css/positioning';
 import 'pivotal-ui/css/colors';
 import 'pivotal-ui/css/whitespace';
 import 'pivotal-ui/css/vertical-alignment';
-import 'pivotal-ui/css/selection';
-import 'pivotal-ui/css/ellipsis';
-import 'pivotal-ui/css/box-shadows';
-import 'pivotal-ui/css/border';
 import 'pivotal-ui/css/alignment';
-import 'pivotal-ui/css/flex-grids';
-import 'pivotal-ui/css/tables';
 import './app.css';
-import logo from './img/telemetry-logo.png';
 
 class Reports extends React.Component {
 
